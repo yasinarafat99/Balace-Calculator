d682790a44e1eaa433463d37fc23913fdaf03b02
yasinarafat99 <newyasinarafatmail@gmail.com>
2023-12-07 21:29:01 +0600

    prblm slvd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import Title from "./component/title";
 import Balance from "./component/Balance";
 import IncomeExpense from "./component/IncomeExpense";
 import TransactionList from "./component/TranscitionList";
 import AddTransaction from "./component/AddTransaction";
+import Title from "./component/title";
 
 function App() {
   return (
