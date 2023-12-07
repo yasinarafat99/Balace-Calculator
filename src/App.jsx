9a8f9cd44938446bd6a18292d42ea6d23fecaf02
yasinarafat99 <newyasinarafatmail@gmail.com>
2023-12-07 21:36:14 +0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import Balance from "./component/Balance";
 import IncomeExpense from "./component/IncomeExpense";
 import TransactionList from "./component/TranscitionList";
 import AddTransaction from "./component/AddTransaction";
-import Title from "./component/title";
 
 function App() {
   return (
     <div>
-      {/* <Title /> */}
       <div className="container">
         <Balance />
         <IncomeExpense />
